fa6cec627b78ab0fdbab077098cac126563cfd6d
hanniabu <hanniabu@gmail.com>
2025-12-09 18:01:59 -0500

    fix new entries endpoint

--- _scripts/new_entries.py
+++ _scripts/new_entries.py
@@ -8,14 +8,8 @@ import os
 import time
 from datetime import datetime, timezone
 import json
-import yaml
 import requests
-from urllib.parse import quote
-from dotenv import load_dotenv
 
-
-load_dotenv()
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 
 
 current_time = round(time.time()) # seconds
@@ -67,44 +61,5 @@ with open(new_entries_path, "w") as new_entries_file:
 with open(old_adoption_list_path, "w") as old_adoption_list_file:
   json.dump(current_adoption_list, old_adoption_list_file)
 
-
-# post new entries to telegram
-new_entries.reverse()
-print(new_entries)
-
-
-for entry in new_entries:
-  if entry['is_recent']:
-    status = ''
-    # if entry['status'] == 'dev':
-    #   status = '[dev] '
-    entities = '/'.join(entry['entities'])
-    products = ', '.join(entry['products'])
-    source = entry['sources'][0]
-    support = ''
-    if entry['chains'][0] != None:
-      chains = ', '.join(entry['chains'])
-      support = f" with support for {chains}"
-    # use endpoint to get channel id after interacting with bot (/start)
-    # https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates
-    chat_list = [
-        "-1002400345737" # @ethereumadoption
-      ]
-    message = f"{status}{entities} announces {products}{support}"
-    message = f"{message.upper()}\n\n{source}"
-    for chat_id in chat_list:
-      url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-      params = {
-        'chat_id': chat_id,
-        'text': message,
-        'reply_markup': json.dumps({
-          'inline_keyboard': [[{
-            'text': 'EthereumAdoption.com',
-            'url': 'https://ethereumadoption.com'
-          }]]
-        })
-      }
-      print(requests.get(url=url, params=params).json())
-      time.sleep(1)
 
 
